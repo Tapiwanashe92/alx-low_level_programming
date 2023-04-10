a8838ca167e81888901ad76c535b6e6c6b8899b3
Tapiwanashe92 <tapiwanashembk@gmail.com>
2023-04-10 14:04:40 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0**/
+
+int main(int argc, char *argv[])
+{
+	int w, mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (w = 1; w < argc; w++)
+		mul *= atoi(argv[w]);
+	printf("%d\n", mul);
+	return (0);
+}
